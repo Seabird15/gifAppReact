1aedb7f9bf4893b21c5774e77255413552faaf6c
2B-0005 <2b-0005@MacBook-Pro-de-IT.local>
2023-02-21 16:47:44 -0300

    styles

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,11 +1,11 @@
-
+import '../styles.css'
 
 export const GifItem = ({title, url, id}) => {
 
     return (
         <div className="card">
             <img src={url} alt={title} />
-            <p>{title}</p>
+            <p className="title">{title}</p>
         </div>
     )
 }
